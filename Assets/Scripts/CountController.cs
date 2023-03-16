c05c0d3d182a142bf247afc58798daf6380ff1f0
Oğuzhan Ergün <Oguzh4n15@hotmail.com>
2023-03-16 21:17:33 +0300

    Karakter scale eklendi
    Karakter scale eklendi

--- Assets/Scripts/CountController.cs
+++ Assets/Scripts/CountController.cs
@@ -7,32 +7,48 @@ public class CountController : MonoBehaviour
 {
     [SerializeField] public TextMeshProUGUI count;
     [SerializeField] public int baslangiccountdegeri;
+    private Animator Animator;
     public int countsayaci;
+    private Transform transformplayer;
+
 
     // Start is called before the first frame update
     void Start()
     {
         countsayaci = baslangiccountdegeri;
         count.text = countsayaci.ToString();
+        Animator = GetComponent<Animator>();
+        transformplayer = GetComponent<Transform>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Wall")
+        if (other.gameObject.tag == "Wall" && !Animator.GetBool("IsDead"))
         {
             countsayaci = countsayaci - other.gameObject.GetComponent<StrognValue>().countstartvalue;
             count.text = countsayaci.ToString();
         }
-        else if (other.gameObject.tag == "Human")
+        else if (other.gameObject.tag == "Human" && !Animator.GetBool("IsDead"))
         {
             Destroy(other.gameObject);
             countsayaci = countsayaci + other.gameObject.GetComponent<StrognValue>().countstartvalue;
             count.text = countsayaci.ToString();
+        }
+
+        transformplayer.localScale = new Vector3(2 + (countsayaci * 0.1f), 2 + (countsayaci * 0.1f), 2 + (countsayaci * 0.1f));
+
+        if (countsayaci <= 0)
+        {
+            Animator.SetBool("IsDead", true);
+            count.text = "0";
         }
     }
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,28 +7,35 @@ public class PlayerController : MonoBehaviour
     public float HorizontalValue;
     public float speed;
     private float BorderX = 1.7f;
+    [SerializeField] private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-  
+        #region Borders 
         if (transform.position.x < -BorderX)
         {
-            transform.position = new Vector3(-BorderX,transform.position.y,transform.position.z);
+            transform.position = new Vector3(-BorderX, transform.position.y, transform.position.z);
         }
-        else if(transform.position.x > BorderX)
+        else if (transform.position.x > BorderX)
         {
             transform.position = new Vector3(BorderX, transform.position.y, transform.position.z);
 
         }
+        #endregion
+        #region Right Left Movement Controller
         HorizontalValue = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * speed * HorizontalValue);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed );
-
+        if (!animator.GetBool("IsDead"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        
+        #endregion
     }
 }
